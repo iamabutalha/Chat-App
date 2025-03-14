2c96947ddb6d789301f5d10caae29bc94ffbacf6
iamabutalha <maabutalhakhalil@gmail.com>
2025-03-14 09:23:16 +0500

    ready for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development"
+    import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
       : "/api",
   withCredentials: true,
